bafd1cf0ffadecdbef8e53e8022f47c651c2b1e0
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-07-04 16:10:42 +0000

    Added url, view, to pets App

--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Pet
 
 # Create your views here.
+
+def pets(request):
+
+    """This view return page this all pets on it"""
+    pets = Pet.objects.all()
+    context = {
+        'pets' : pets,
+    }
+
+    return render(request, 'pets/pets.html', context)
