68b04b3ed427e9e22171c48a2652c153bfc4824c
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-07-04 15:50:57 +0000

    Customized Admin

--- pets/admin.py
+++ pets/admin.py
@@ -2,5 +2,24 @@ from django.contrib import admin
 from .models import Pet, Category
 
 # Register your models here.
-admin.site.register(Pet)
-admin.site.register(Category)
+
+class PetAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'category',
+        'gender',
+        'age',
+        'image'
+    )
+
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+    )
+
+admin.site.register(Pet, PetAdmin)
+admin.site.register(Category, CategoryAdmin)
