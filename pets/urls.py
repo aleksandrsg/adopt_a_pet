8dd9c3116bc14f4cd069dcfb5399d811da42e393
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-07-05 12:56:11 +0000

    updated requirements, pet card, view and url

--- pets/urls.py
+++ pets/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.pets, name='pets'),
+    path('<pet_id>', views.pet_desc, name='pet_desc'),
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Pet
 
 # Create your views here.
@@ -12,3 +12,14 @@ def pets(request):
     }
 
     return render(request, 'pets/pets.html', context)
+
+
+def pet_desc(request, pet_id):
+
+    """This view return pet description"""
+    pet = get.object_or_404(Pet, pk=pet_id)
+    context = {
+        'pet' : pet,
+    }
+
+    return render(request, 'pets/pet_desc.html', context)
