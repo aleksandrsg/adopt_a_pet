b5aa513d7809a4a4c2128a28c1077bb031e15e18
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-07-13 22:18:11 +0000

    Signup Form tested and works

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def signup(request):
-    form = UserCreationForm()
-    context = {
-        'form':form
-    }
-    return render(request, 'accounts/signup.html', context)
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            return redirect('pets')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form':form })
