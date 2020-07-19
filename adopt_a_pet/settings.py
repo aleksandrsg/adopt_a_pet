759022d0aa21c404d3473fe268dc6a70afd30726
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-07-19 22:10:39 +0000

    Added dj database url settings

--- adopt_a_pet/settings.py
+++ adopt_a_pet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,12 +81,16 @@ WSGI_APPLICATION = 'adopt_a_pet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': dj_database_url.parse('postgres://aocckdyaeqwtmy:1b0a21847b195f9627850c9f0f9609a4281001bb72fe407df690de20b3bd8815@ec2-54-246-87-132.eu-west-1.compute.amazonaws.com:5432/d4kojmhdfv5lg8')
     }
-}
 
 
 # Password validation
