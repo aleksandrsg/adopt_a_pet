615e315ab6758a23d93e678b59ba09f6483e319a
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-07-21 20:26:33 +0000

    Added root_static

--- adopt_a_pet/settings.py
+++ adopt_a_pet/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5j-n4m=@$s&9#)ojahe9@#s(v-q0)7ff9&&ep!8rm+i^#d&i@)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['adopt-a-pet-1.herokuapp.com', 'localhost']
 
@@ -130,8 +130,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
