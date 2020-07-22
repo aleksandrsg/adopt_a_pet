2b4fce4a014f61f5407fe54150d91047c163acd1
aleksandrsg <aleksandrs.galiullins@gmail.com>
2020-07-22 19:04:45 +0000

    Generated and changed new secret key

--- adopt_a_pet/settings.py
+++ adopt_a_pet/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5j-n4m=@$s&9#)ojahe9@#s(v-q0)7ff9&&ep!8rm+i^#d&i@)'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
